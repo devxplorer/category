ec494a5e826df943a0c383276636a6458c90f2fe
devxplorer <devxplorer@gmail.com>
2017-09-22 19:24:33 +0300

    unnecessary order by

--- category/helpers.py
+++ category/helpers.py
@@ -22,7 +22,6 @@ def get_categories_raw_sql(outfit_id, user_id):
           ON category_category.id = category_category_outfits.category_id 
              AND category_category_outfits.outfit_id=%s
     WHERE category_category.owner_id=%s
-    GROUP BY category_category.id, category_category.name
-    ORDER BY category_category.id"""
+    GROUP BY category_category.id, category_category.name"""
     categories = Category.objects.raw(q, [outfit_id, user_id])
     return [{'id': c.id, 'name': c.name, 'added': c.added} for c in categories]
